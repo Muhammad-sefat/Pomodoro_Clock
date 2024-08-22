541137ebaa802d7e073a1438fdeddd630a9b037d
Muhammad Sefat <muhammadsefat55@gmail.com>
2024-08-22 20:37:43 +0600

    Complete Start button and dynamic all text

--- script.js
+++ script.js
@@ -4,13 +4,75 @@ const startBtn = document.querySelector(".startBtn");
 const pauseBtn = document.querySelector(".pauseBtn");
 const resumeBtn = document.querySelector(".resumeBtn");
 const ResetBtn = document.querySelector(".ResetBtn");
+const countPomoDisplay = document.querySelector(".countPomoDisplay");
 
-// function for start btn
-const startButton = () => {
-  alert("started");
+// variables
+const WORK_TIME = 1 * 60;
+const BREAK_TIME = 0.5 * 60;
+let timerID = null;
+let oneRoundComplete = false;
+let countTimer = 0;
+
+// function for save localstroage
+const saveLocalCounts = () => {
+  let counts = JSON.parse(localStorage.getItem("pomoCounts"));
+  counts !== null ? counts++ : (counts = 1);
+  localStorage.setItem("pomoCounts", JSON.stringify(counts));
+};
+
+// function for updateTitle
+const updateTitle = (msg) => {
+  tittle.textContent = msg;
+};
+
+// function for countDown
+const countDown = (time) => {
+  return () => {
+    timer.textContent = time;
+    time--;
+    if (time <= 0) {
+      stopTimer();
+      if (!oneRoundComplete) {
+        timerID = startTimer(BREAK_TIME);
+        oneRoundComplete = true;
+        updateTitle("It's Break Time!");
+      } else {
+        updateTitle("Completed One Round of Pomodoro Technique!");
+        setTimeout(() => updateTitle("Start Timer Again", 2000));
+        countTimer++;
+        saveLocalCounts();
+        showPomoCounts();
+      }
+    }
+  };
+};
+
+// function for startTimer
+const startTimer = (startTime) => {
+  if (timerID !== null) {
+    stopTimer();
+  }
+  return setInterval(countDown(startTime), 1000);
+};
+
+// function for stopTimer
+const stopTimer = () => {
+  clearInterval(timerID);
+  timerID = null;
 };
 
 // add eventlistener
 startBtn.addEventListener("click", () => {
-  startButton();
+  timerID = startTimer(WORK_TIME);
+  updateTitle("It's Work Time!");
 });
+
+// function for show pomoCounts
+const showPomoCounts = () => {
+  const counts = JSON.parse(localStorage.getItem("pomoCounts"));
+  if (counts > 0) {
+    countPomoDisplay.style.display = "flex";
+  }
+  countPomoDisplay.firstElementChild.textContent = counts;
+};
+showPomoCounts();
